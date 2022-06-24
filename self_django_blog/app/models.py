0f70a4660626956079731162305ffcb8f135f88a
unohayato <mamidori1p1p@gmail.com>
2022-06-25 01:33:07 +0900

    モデルの追加

--- self_django_blog/app/models.py
+++ self_django_blog/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models
+
+class Post(models.Model):
+  title = models.CharField(verbose_name="タイトル",max_length=255, blank=False)
+  content = models.TextField(verbose_name="投稿内容",blank=False)
+  created_at = models.DateTimeField(verbose_name="作成日時",auto_now_add=True)
+  updated_at = models.DateTimeField(verbose_name="変更日時",auto_now=True)
+  
--- self_django_blog/app/views.py
+++ self_django_blog/app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-class IndexView(View):
+class IndexView(TemplateView):
   template_name = 'app/index.html'
-  
-  def get(self, request, *args, **kwargs):
-    return render(request, 'app/index.html', )
+
+ 
