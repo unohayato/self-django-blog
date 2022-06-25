39b08ce1825afe0f5c8d9a39e8998d172a0afa46
unohayato <mamidori1p1p@gmail.com>
2022-06-25 20:06:03 +0900

    モデルをview経由で表示

--- self_django_blog/app/urls.py
+++ self_django_blog/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.Index.as_view(), name='index'),
+    path('detail/<pk>/', views.Detail.as_view(), name='detail'),
 ]
--- self_django_blog/app/views.py
+++ self_django_blog/app/views.py
@@ -1,10 +1,17 @@
+from pyexpat import model
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView,DetailView
+from .models import Post
 
 # Create your views here.
 
 
-class IndexView(TemplateView):
-  template_name = 'app/index.html'
+class Index(ListView):
+  model = Post
+  
+class Detail(DetailView):
+  model = Post
+  
+  
 
  
